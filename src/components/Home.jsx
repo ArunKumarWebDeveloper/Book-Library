8f684de9bb0e7872616ea23de6424f5a4ad8898b
Arun <arunlokesh78@gmail.com>
2025-09-30 15:59:58 +0530

    updated-codes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,12 +3,36 @@ function Home() {
   
 
   return (
-    <div>
+    <div className="home">
+      <div className="Search">
+        <img src="./public/user.png" alt="user" />
+       <div className="user">
+        <nav>
+          <ul>
+            <li><h4>Book Library</h4></li>
+          </ul>
+        </nav>
+       </div>
        
-      <h1>Home page</h1>
+      <div className="search-bar">
+        <input type="text" placeholder="Search for books..." />
+        <button>Search</button>
+        <h2>Search the Books You Love</h2>
+        <footer>@Book Library 2025  <a href="https://developers.google.com/books">Link to API</a></footer>
+       </div>
+       
+      </div>
+       
+
+      <div className="Content">
+      
+      </div>
       
     </div>
+    
   );
 }
 
 export default Home;
+
+

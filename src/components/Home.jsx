89f0c2f2836a4d95d1de35a9fbe87ccc893c22c5
Arun <arunlokesh78@gmail.com>
2025-10-01 12:40:53 +0530

    updated-codes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,38 +1,36 @@
 
 function Home() {
-  
-
   return (
     <div className="home">
-      <div className="Search">
-        <img src="./public/user.png" alt="user" />
-       <div className="user">
-        <nav>
-          <ul>
-            <li><h4>Book Library</h4></li>
-          </ul>
-        </nav>
-       </div>
-       
-      <div className="search-bar">
-        <input type="text" placeholder="Search for books..." />
-        <button>Search</button>
-        <h2>Search the Books You Love</h2>
-        <footer>@Book Library 2025  <a href="https://developers.google.com/books">Link to API</a></footer>
-       </div>
-       
-      </div>
-       
+      <img className="main-img" src="main.jpg" alt="Main" />
+      <nav className="overlay-nav">
+        <div className="logo">
+          <img src="icon.png" alt="Icon" />
+        </div>
+        <ul>
+          <li className="li1">Explore</li>
+          <li>About</li>
+        </ul>
+        <div className="auth-buttons">
+          <button>Login</button>
+          <button>Sign Up</button>
+        </div>
+      </nav>
 
-      <div className="Content">
-      
+      <div className="overlay-content">
+        <h2>Best Online <br />Learning Book Library</h2>
+        <p>Sign up today and start exploring your next favorite read.</p>
+        <button className="cta-btn">Get Started</button>
       </div>
-      
+        <footer className="overlay-footer">
+        <span> Book Library 2025  &copy; All rights reserved.</span>
+        <p><a href="https://developers.google.com/books">Google Books API</a></p>
+      </footer>
     </div>
-    
   );
 }
 
 export default Home;
+
 
 

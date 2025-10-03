4780e8a95842bbe84d902923fcb3c67da5501b46
Arun <arunlokesh78@gmail.com>
2025-10-03 09:39:32 +0530

    updated-codes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,10 @@ const Footer =() =>{
         <div >
  <footer id="about" className="overlay-footer">
         <span> Book Library 2025  &copy; All rights reserved.</span>
-        <div>
-        <p>Developed By Arun.R </p>
-        <a href="mailto:arunlokesh78@gmail.com" target="_blank">Contact through mail</a>
-        <p>GitHub</p>
-        <p>LinkedIn</p>
-        </div>
+     
+        <p>Developed By Arun Kumar R </p>
+        <abbr title="Mail"><a href="mailto:arunlokesh78@gmail.com" target="_blank">Contact through mail</a></abbr>
+     
         <p><a href="https://developers.google.com/books" target="_blank">&copy; Credit's to Google Books API</a></p>
       </footer>
         </div>
